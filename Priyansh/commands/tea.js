6170a39dc9d7d748faa262b1653cba1c5c0cd7fd
Ahmee786 <ahmadzyz4@gmail.com>
2024-11-30 14:40:18 +0500

    Add files via upload

---
+++ Priyansh/commands/tea.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+name: tea.
+
+description: 'Sends a random picture when user uses to command.",
+
+async execute(message) {
+
+try
+
+const response await fetch('https://imgur.com/a/g8hHN4k.jpg');
