55673fbbe907a573730814eed64c97a9991fbb1b
Ahmee786 <ahmadzyz4@gmail.com>
2024-12-05 08:48:42 +0500

    Update uptime.js

--- Priyansh/commands/uptime.js
+++ Priyansh/commands/uptime.js
@@ -7,7 +7,7 @@ module.exports = {
   config: {
     name: "uptime",
     version: "1.0.0",
-    hasPermssion: 0,
+    hasPermssion: 2,
     credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
     description: "test",
     commandCategory: "box",
